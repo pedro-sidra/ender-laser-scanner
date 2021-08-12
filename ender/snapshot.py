eb927aef4a5559708c336b9bfbc2258e4aff0e92
Pedro Freitas <pedrosidra0@gmail.com>
2021-08-12 19:11:51 -0300

    Add rgb parameter to snapshotter

--- ender/snapshot.py
+++ ender/snapshot.py
@@ -5,9 +5,10 @@ import cv2
 
 
 class MjpgSnapshotter():
-	def __init__(self, url, snapshot_params={"action":"snapshot"}) -> None:
+	def __init__(self, url, snapshot_params={"action":"snapshot"}, rgb=True) -> None:
 	    self.url = url
 	    self.params = snapshot_params
+	    self.rgb=rgb
 	
 	def get_raw(self):
 		r = requests.get(self.url, params=self.params, stream=True)
@@ -17,10 +18,15 @@ class MjpgSnapshotter():
 			raise Exception("Response not ok!", r.status_code)
 
 	def get_img(self):
-		return cv2.imdecode(
+		im = cv2.imdecode(
 			np.asarray(bytearray(b"".join(self.get_raw())),
 				   dtype=np.uint8),
 			flags=-1)
+		
+		if self.rgb:
+			return cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+		else:
+			return im
 	def read(self):
 		try:
 			return True, self.get_img()
