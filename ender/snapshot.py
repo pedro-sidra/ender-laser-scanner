d7331187f6a126d0ec5bf2952b7964024089c0b3
Pedro Freitas <pedrosidra0@gmail.com>
2021-08-15 10:39:44 -0300

    add MjpgSnapshotterExp that controls cam parameters

--- ender/snapshot.py
+++ ender/snapshot.py
@@ -1,4 +1,6 @@
 import requests
+import subprocess
+import time
 import shutil
 import numpy as np
 import cv2
@@ -32,6 +34,78 @@ class MjpgSnapshotter():
 			return True, self.get_img()
 		except:
 			return False, None
+
+class MjpgSnapshotterExp(MjpgSnapshotter):
+	"""Extends `MjpgSnapshotter` by adding v4l2 commands to the host device
+
+	Can adjust exposure via `set_exposure`, gain via `set_gain`
+	and others via `send_command`, to get available commands call `get_commands`
+	"""
+	def __init__(self, url,
+				 snapshot_params={"action":"snapshot"}, 
+				 rgb=True,
+				 ssh_str=None) -> None:
+		"""Init a virtual camera that GETs frames from `url` with `snapshot_params`,
+		and can send camera parameter commands by SSH connection using `ssh_str`
+
+		Always uses the first available camera from v4l2-ctl
+		@param url url hosting the webcam API
+		@snapshot_params params to use at the API to the webcam
+		@param rgb whether to convert the image from opencv's BGR to matplotlib's RGB
+		@ssh_str acess str to use for ssh e.g. pi@ender3.local
+		"""
+		super().__init__(url, snapshot_params=snapshot_params, rgb=rgb)
+		self.ssh_str = ssh_str
+		# Turn off auto-exp
+		self._ssh_command("v4l2-ctl -cexposure_auto=1")
+		# Turn off white balance
+		self._ssh_command("v4l2-ctl -cwhite_balance_temperature_auto=0")
+		self._ssh_command("v4l2-ctl -cwhite_balance_temperature=0")
+
+	def _ssh_command(self, cmd):
+		"""Send ssh command to host"""
+		try:
+			return subprocess.check_output([
+					"ssh",
+					self.ssh_str,
+					cmd
+				]).decode()
+		except subprocess.CalledProcessError:
+			time.sleep(0.5)
+			return subprocess.check_output([
+					"ssh",
+					self.ssh_str,
+					cmd
+				]).decode()
+	
+	def set_exposure(self, value, wait=True):
+		"""Set the exposure using -cexposure=value
+		@param wait whether to wait a bit before returning for exposure to register"""
+		ret =  self._ssh_command(f"v4l2-ctl -cexposure_absolute={value}")
+		if wait:
+			time.sleep(0.5)
+		return ret
+
+	def set_gain(self, value, wait=True):
+		"""Set the gain using -cgain=value
+		@param wait whether to wait before returning for gain to register"""
+		ret =  self._ssh_command(f"v4l2-ctl -cgain={value}")
+		if wait:
+			time.sleep(0.5)
+		return ret
+
+	def send_command(self, flags):
+		"""Send get/set command to `v4l2-ctl` in the host
+		(pass flags and command, e.g. `-cgain=255` or `-Cgain`)"""
+		return self._ssh_command(f"v4l2-ctl {flags}").split("Flags:")[-1]
+
+	def get_commands(self):
+		"""Return list of available flags from v4l2-ctl"""
+		return self._ssh_command("v4l2-ctl --all").split("Flags:")[-1]
+
+
+print()
+print()
 
 
 if __name__=="__main__":
